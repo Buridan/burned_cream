60bd7a8847003f05711f9ffd76486877204c2a2e
BusyVar <paul.lett@free.fr>
2018-12-17 20:46:23 +0100

    add purpose for sprite lib

--- Fenetre/src/sprite.h
+++ Fenetre/src/sprite.h
@@ -1,7 +1,13 @@
 #include <SDL2/SDL.h>
-
+/*
+	@author : Busyvar
+	@brief : goal of this lib is to make a texture for any kind of visual game element.
+*/
+//TODO : utiliser sprite.c pour charger indifferemment le decors des autres images
+//public
 void initSprite();
 SDL_Texture* getTexSprite();
 
+//private
 void _loadTexture();
 void _loadSprite();
