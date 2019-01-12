6a82f8c3cc3773850b6a5df75b61f919e5ae81af
PE LETT <paul.lett@free.fr>
2019-01-12 23:41:36 +0100

    remove useless prototype

--- Fenetre/src/main.h
+++ Fenetre/src/main.h
@@ -3,7 +3,6 @@
 #include "character.h"
 
 int mainEventLoop();
-void renderForeground(Foreground* fg);
 void renderBackground(Background* bg);
 void refreshAnimation(unsigned char fps);
 void setDirection(const byte_t dirMask);
