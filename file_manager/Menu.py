ffe81f78a2a3dd8a76d0f985fce6a24c84943d82
U-PARKEON\pelett <pelett@PC12825-BES.dynamic.besancon.parkeon.com>
2019-02-27 14:01:08 +0100

    file manager from work is superior

--- file_manager/Menu.py
+++ file_manager/Menu.py
@@ -1,5 +1,5 @@
 import curses
-from os import listdir, getcwd, system, chdir, path, listdir
+from os import curdir, listdir, getcwd, system, chdir, path, listdir
 from os.path import isfile
 
 class Menu:
@@ -20,7 +20,8 @@ class Menu:
 	def display(self):
 		self.screen.addstr(0, self.x, self.name, curses.A_NORMAL)
 		for item in self.menu_array:
-			self.screen.addstr(item.getLine(), self.x , item.getName(),curses.color_pair(2) if item.isSelected() else curses.A_NORMAL)
+			self.screen.addstr(item.getLine(), self.x , item.getName().ljust(60),
+							curses.color_pair(2) if item.isSelected() else curses.A_NORMAL)
 	def nextItem(self):
 		if self.menu_index != len(self.menu_array)-1:
 			self.menu_array[self.menu_index].setSelected(False)	
@@ -34,9 +35,7 @@ class Menu:
 			self.menu_array[self.menu_index].setSelected(True)	
 			self.display()
 	def moveOut(self):
-		self.screen.clear()
-		chdir("..")
-		self.setName(getcwd())
+		self.setName(getcwd()+"../")
 		self.menu_array = []
 		self.menu_index = 0
 		self.nextLine = 1
@@ -45,25 +44,45 @@ class Menu:
 		self.display()
 	def moveIn(self):
 		self.screen.clear()
-		self.menu_array[self.menu_index].execute()
-		self.setName(getcwd())
-		self.menu_array = []
-		self.menu_index = 0
-		self.nextLine = 1
-		for f in listdir("."):
-			self.addItem(f)
-		self.display()
+		if self.menu_array[self.menu_index].type :
+			self.menu_array[self.menu_index].execute()
+			return ""
+		else:
+			self.setName(getcwd())
+			self.menu_array = []
+			self.menu_index = 0
+			self.nextLine = 1
+			for f in listdir("."):
+				self.addItem(f)
+			self.display()
+			return getcwd() + self.menu_array[self.menu_index].name
 	def setName(self,newName):
 		self.name = newName
 	def setPosition(self,x):
 		self.x = x
+	def upDir(self):
+		self.name+="/../"
+	def execute(self):
+		self.menu_array[menu_index].execute()
+	def searchChildItem(self,path):
+		for item in self.menu_array:
+			if item.name == path:
+				item.setSelected(False)
+				
 
-class MainMenu(Menu):
+
+class CurrentMenu(Menu):
 	def __init__(self,menuName,screen,positionX):
 		Menu.__init__(self,menuName,screen,positionX)
 class ParentMenu(Menu):
 	def __init__(self,menuName,screen,positionX):
 		Menu.__init__(self,menuName,screen,positionX)
+		chdir(self.name + "/../")
+		self.name = getcwd()
+	def addItem(self, item_name):
+		self.menu_array.append(MenuItem(item_name, self.nextLine))
+		self.nextLine +=1
+		self.menu_array[0].setSelected(True)
 class ChildMenu(Menu):
 	def __init__(self,menuName,screen,positionX):
 		Menu.__init__(self,menuName,screen,positionX)
--- file_manager/Panel.py
+++ file_manager/Panel.py
@@ -1,33 +1,42 @@
-from os import getcwd
-from Menu import ParentMenu, ChildMenu, MainMenu
+from os import listdir, getcwd, system, chdir, path, listdir
+from Menu import Menu, ParentMenu, ChildMenu, CurrentMenu
 from screen import ScreenEnvironment
 
 class Panel():
 	def __init__(self,screen_environment):
-		self.width = 20
+		self.width = 60
 		self.menu_index = -1
 		self.menu_list = []
 		self.scr_env = screen_environment
 		self.addMenus(getcwd())
 	def addMenus(self, path):
-		self.addParentMenu(path+"/../",0)
-		self.addMainMenu(path,((self.scr_env.w/2)-(self.width/2)))
-		self.addChildMenu(path,self.scr_env.w - self.width)
-	def addParentMenu(self, path,posX):
 		self.menu_index+=1
-		self.menu_list.append(ParentMenu(path,self.scr_env.screen, posX))
-	def addMainMenu(self, path,posX):
+		self.menu_list.append(ParentMenu(path+"/../",self.scr_env.screen, 0))
 		self.menu_index+=1
-		self.menu_list.append(MainMenu(path,self.scr_env.screen, posX))
-	def addChildMenu(self, path,posX):
-		self.menu_index+=1
-		self.menu_list.append(ChildMenu(path,self.scr_env.screen, posX))
+		self.menu_list.append(CurrentMenu(path,self.scr_env.screen, self.getCenteredBlock(self.width)))
+		self.menu_list[0].searchChildItem(path)
+		#self.menu_index+=1
+		#self.menu_list.append(ChildMenu(path,self.scr_env.screen,self.alignRight(self.width)))
 	def getFocusedMenu(self):
 		return self.menu_list[1]
 	def display(self):
 		for f in range(self.menu_index):
 			self.menu_list[f].display()
 	def goLeft(self):
-
-		self.getFocusedMenu().setPosition(self.scr_env.w - self.width)
-		self.display()
+		self.menu_list=[]
+		self.menu_index = 1
+		chdir("..")
+		self.addMenus(getcwd())
+		#self.scr_env.screen.clear()
+		#for f in range(self.menu_index):
+		#	self.menu_list[f].moveOut()
+		#self.display()
+	def goRight(self):
+		self.menu_list=[]
+		self.menu_index = 1
+		chdir(getcwd() + self.menu_list[1].moveIn())
+		self.addMenus(getcwd())
+	def getCenteredBlock(self,width):
+		return (self.scr_env.w>>1)-(width>>1)
+	def alignRight(self,width):
+		return self.scr_env.w - self.width
