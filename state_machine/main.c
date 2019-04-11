78ebf40cf89e08ae51538276600a37c5b3283a41
Busyvar <paul.lett@free.fr>
2019-04-11 07:00:59 +0000

    separate state machine unit test from main

--- state_machine/main.c
+++ state_machine/main.c
@@ -1,31 +1,58 @@
-#!tcc -run
+#!/usr/bin/tcc -run
 #include "main.h"
+#include <sys/types.h>
+#ifdef _WIN32
+#include <windows.h>
+#include <winsock.h>
+#elif unix
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#endif
 
+#define MSGQID 80
+
+typedef struct
+{
+	long mtype;
+	char mtext[256];
+}msgbuf;
+int unitTestStateMachine();
 int main()
 {
 	LOGFN;
-	message_t request = {MY_PUID,MSG_HELLO};
-	if(E_OK != pushMsg(request))
-		printf("error while sending message %d",request.message);
-	request.message = MSG_HOLA;
-	pushMsg(request);
-	request.message = MSG_FOO;
-	pushMsg(request);
-	request.message = MSG_HOLA;
-	pushMsg(request);
-	_rollMsgQueue();
-	request.message = MSG_HELLO;
-	pushMsg(request);
-	request.message = MSG_RUN;
-	pushMsg(request);
-	request.message = MSG_FINISH;
-	pushMsg(request);
-	request.message = MSG_RESET;
-	pushMsg(request);
-	request.message = MSG_STOP;
-	pushMsg(request);
-	_rollMsgQueue();
+
+	msgbuf message = (msgbuf){.mtype=1,.mtext={0,}};
+	size_t szMsgText = sizeof(message.mtext);
+
+	key_t ipcKey = ftok("ipc.ftok",MSGQID);
+	msgsnd(MSGQID, &message, szMsgText,IPC_NOWAIT);
+	printf("ipcKey:%d\n",msgget(ipcKey,IPC_CREAT));
 
 	return 0;
 }
 
+int unitTestStateMachine()
+{
+	int errCnt=0;
+	message_t request = {MY_PUID,MSG_HELLO};
+	errCnt += pushMsg(request);
+	errCnt += request.message = MSG_HOLA;
+	errCnt += pushMsg(request);
+	errCnt += request.message = MSG_FOO;
+	errCnt += pushMsg(request);
+	errCnt += request.message = MSG_HOLA;
+	errCnt += pushMsg(request);
+	errCnt += _rollMsgQueue();
+	errCnt += request.message = MSG_HELLO;
+	errCnt += pushMsg(request);
+	errCnt += request.message = MSG_RUN;
+	errCnt += pushMsg(request);
+	errCnt += request.message = MSG_FINISH;
+	errCnt += pushMsg(request);
+	errCnt += request.message = MSG_RESET;
+	errCnt += pushMsg(request);
+	errCnt += request.message = MSG_STOP;
+	errCnt += pushMsg(request);
+	errCnt += _rollMsgQueue();
+	return errCnt ? E_ERR : E_OK;
+}
